449a690e84dd4eeb709f03e4251bb46d59e67927
paulgastongouron <paul-gaston.gouron@paymium.com>
2016-08-28 16:26:57 +0200

    Add client-side app initialization logic

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -26,3 +26,55 @@ function processUserInput(chatApp, socket) {
 	}
 	$('#send-message').val('');
 }
+
+var socket = io.conntect();
+
+$(document).ready(function() {
+	var chatApp = new Chat(socket);
+	// Display results of a name-change attempt
+	socket.on('nameResult', function(result) {
+		var message;
+		if (result.success) {
+			message = 'You are now know as ' + result.name + '.';
+		} else {
+			message = result.message;
+		}
+		$('#messages').append(divSystemContentElement(message));
+	});
+	// Display result of a room change
+	socket.on('joinResult', function(result) {
+		$('#room').text(result.room);
+		$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+	// Display recived message
+	socket.on('message', function(message) {
+		var newElement = $('<div></div>').text(message.text);
+		$('#messages').append(newElement);
+	});
+	// Display list of available rooms
+	socket.on('rooms', function(rooms) {
+		$('#room-list').empty();
+		for (var room in rooms) {
+			room = room.substring(1, room.length);
+			if (room != '') {
+				$('#room-list').append(divEscapedContentElement(room));
+			}
+		}
+		// Allow user to change room by click room name
+		$('#room-list div').click(function() {
+			chatApp.processCommand('/join' + $(this).text());
+			$('#send-message').focus();
+		});
+	});
+	// Request list of available rooms
+	setInterval(function() {
+		socket.emit('rooms');
+	}, 1000);
+	// Go fancy focusing the name
+	$('#send-message').focus();
+	// Allow submit form to send message
+	$('#send-form').submit(function() {
+		processUserInput(chatApp, socket);
+		return false
+	});
+});
